8217fb0b9fc32312b79ba7d3ee94c55cd5af4755
RadojkoRadulovic <radoykoradulovic@gmail.com>
2022-12-07 21:31:46 -0500

    added some comments

--- src/App.js
+++ src/App.js
@@ -1,132 +1,106 @@
-import app from './firebase.js';
-import { useState, useEffect } from 'react';
-import { getDatabase, ref, onValue, push, remove } from 'firebase/database';
-import Form from './Form.js';
-import './App.css';
+import app from "./firebase.js";
+import { useState, useEffect } from "react";
+import { getDatabase, ref, onValue, push, remove } from "firebase/database";
+import Form from "./Form.js";
+import "./App.css";
 
 function App() {
-  // Step 2: Set up a stateful variables to hold our To do list
+  
+  // Step 2: Set up a stateful variables to hold To do list item
   const [toDoItems, setToDoItem] = useState([]);
-
-  const [userInput, setUserInput] = useState( '' );
-
-  useEffect( () => {
+  //Set up a stateful variables to hold  user input
+  const [userInput, setUserInput] = useState("");
+  
+  // Collect data from database
+  useEffect(() => {
     const database = getDatabase(app);
     const dbRef = ref(database);
-    
 
     onValue(dbRef, (response) => {
-      // console.log(response.val());
-
       const data = response.val();
-
-      const updatedDbInfo = []
-
+      const updatedDbInfo = [];
       for (let key in data) {
-        
-        updatedDbInfo.push({key: key, name:data[key]});
+        updatedDbInfo.push({ key: key, name: data[key] });
       }
-
-      setToDoItem(updatedDbInfo);      
-
-    })
-  }, [] );
+      setToDoItem(updatedDbInfo);
+    });
+  }, []);
 
   const handleInputChange = (event) => {
     setUserInput(event.target.value);
-  }
-
+  };
+  // Push user input into our db
   const handleSubmit = (event) => {
     event.preventDefault();
 
     const database = getDatabase(app);
-    const dbRef = ref(database); 
+    const dbRef = ref(database);
 
-    setUserInput('');
+    setUserInput("");
 
-    if(userInput.length !== 0) {
+    if (userInput.length !== 0) {
       push(dbRef, userInput);
     } else {
-      alert('pleaase put some text into form');
+      alert("Pleaase put some text into text area");
     }
-
-  }
-
-  const handleRemoveToDoItem = (toDoKey) => {
+  };
+    //  Remove each item from TO DO LIST
+    const handleRemoveToDoItem = (toDoKey) => {
     const database = getDatabase(app);
     const dbRef = ref(database, `/${toDoKey}`);
 
-    remove(dbRef)
-  }
+    remove(dbRef);
+  };
 
   return (
     <div className="App">
-        <header>
-          <h1>Make your priority list for today</h1>
-        </header>
+      <header>
+        <h1>Make your priority list for today</h1>
+      </header>
       <div className="wrapper">
         <main>
-        <section className="form">
-          <Form 
-          handleInputChange={handleInputChange}
-          userInput={userInput}
-          handleSubmit={handleSubmit}
-          />
-          <div className="alert">
-            <p>Please enter text into tex area</p>
-            <span>X</span>
-          </div>
-        </section>
-        <section className="list">
-          <ol>
-            {toDoItems.map( (toDo) => {
-              return (   
-                <div className="listContainer">
-                <li key={toDo.key}>
-                  <p>{toDo.name} | {toDo.key} </p>
-                </li>
-                <button onClick={ () => handleRemoveToDoItem(toDo.key)} className="removeBotton">X</button>
-                </div>             
-              )
-            } )}
-          </ol>
-        </section>
+          <section className="form">
+            <Form
+              handleInputChange={handleInputChange}
+              userInput={userInput}
+              handleSubmit={handleSubmit}
+            />
+          </section>
+          <section className="list">
+            <ol>
+              {/* Map through it and display the TO DO LIST on the page */}
+              {toDoItems.map((toDo) => {
+                return (
+                  <div className="listContainer">
+                    <li key={toDo.key}>
+                      <p>
+                        {toDo.name}
+                      </p>
+                    </li>
+                    <button
+                      onClick={() => handleRemoveToDoItem(toDo.key)}
+                      className="removeBotton"
+                    >
+                      X
+                    </button>
+                  </div>
+                );
+              })}
+            </ol>
+          </section>
         </main>
-      </div> {/*End .wrapper */}
+      </div>{" "}
+      {/*End .wrapper */}
       <footer>
-        <p>Copyright&copy; <a href="https://junocollege.com/">Juno College of Technology </a>
-            Created by <a href="https://radoyko.com">Radojko Radulovic</a></p>
+        <p>
+          Copyright&copy;{" "}
+          <a href="https://junocollege.com/">Juno College of Technology </a>
+          Created by <a href="https://radoyko.com">Radojko Radulovic</a>
+        </p>
       </footer>
-
     </div>
   );
 }
 
 export default App;
 
-
-
-
-// Step 1: Import our app from firebase.js
-        // Import useState and useEfect
-// Step 2: Set up a stateful variables to hold our To do list
-
-// Step 3: Map through it and display the TO DO LIST on the page
-
-// Step 4: Collect our data from our database, Then push the information into our stateful variables
-
-// Step 5: When we have successfully retrived our data, Store our results in a variable.
-
-// Step6: Our data is an object that contains our TO DO LIST, then loop through our object and push each to an array
-
-// Step 7: Add a form  for users to add TO DO LIST themselves
-
-// Step 8: Create  function that we can use on our input to record the user's input
-
-// Step 9: Create a new statefull veriable to store users input
-
-// Step 10: Bind stateful variable  to a value attribute in our input
-
-// Step 11: Push our user input into our db
-
-// Step 12: Remove each item from TO DO LIST 
